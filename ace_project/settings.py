c9625d412c3d6ce8edc9a6b214efd1bc7c92646e
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2023-10-26 18:03:21 -0300

    add url docs - coreapi(#6)

--- ace_project/settings.py
+++ ace_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'coreapi',
     'ace_api'
 ]
 
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# complemento do coreapi
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}

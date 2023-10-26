c28543aafb1b37446f4391ad2d7fcc7b73def7e5
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2023-10-26 20:02:31 -0300

    add delete e update custom(#7)

--- ace_api/urls.py
+++ ace_api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from ace_api.viewsets import AceViewSet
+from ace_api.viewsets import AceViewSet, AceListAllViewSet, AceRestoreViewSet
 
 router = routers.DefaultRouter()
 router.register(r'ace', AceViewSet)
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('', include(router.urls)),
+    path('list', AceListAllViewSet.as_view({'get': 'list'}), name='list'),
+    path('restore/<int:pk>/', AceRestoreViewSet.as_view(), name='restore'),
 ]
--- ace_api/viewsets.py
+++ ace_api/viewsets.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializer import AceSerializer
 from .models import Ace
+from django.shortcuts import redirect
+from rest_framework import generics, mixins
 
 
 class AceViewSet(ModelViewSet):
@@ -10,3 +12,34 @@ class AceViewSet(ModelViewSet):
     # para pegar somente os ativos
     queryset = Ace.objects.filter(is_active=True)
     serializer_class = AceSerializer
+
+    # sobrescreve o DELETE e so muda o is_ative pra False
+    def destroy(self, request, *args, **kwargs):
+        ace = self.get_object()
+        ace.is_active = False
+        ace.save()
+
+        # return http.HttpResponseRedirect(redirect_to='ace-list')
+        return redirect('/api/v1/')
+
+
+# listar todos, inclusive is_active = False
+class AceListAllViewSet(ModelViewSet):
+    queryset = Ace.objects.all()
+    serializer_class = AceSerializer
+
+#
+
+
+class AceRestoreViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Ace.objects.all()
+    serializer_class = AceSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        ace = self.get_object()
+        ace.is_active = True
+        ace.save()
+        return redirect('/api/v1/')
